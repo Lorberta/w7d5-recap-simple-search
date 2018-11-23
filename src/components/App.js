2e941f382b3e9ec52139143ab26b9541ab7f8552
Maxence Bouret <maxence.bouret@gmail.com>
2018-11-23 10:49:58 +0100

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import CoolSearch from './CoolSearch'
+import jsonStudents from '../students'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      search: ''
+    }
+  }
+  handleSearch = (newSearch) => {
+    this.setState({
+      search: newSearch
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>
-          <img src={logo} alt="Logo" style={{height: 50}}/>
           ReactStudents
         </h1>
-        <CoolSearch />
+        <CoolSearch
+          search={this.state.search}
+          onSearch={this.handleSearch}
+        />
+        <ul>
+          {jsonStudents
+            .filter(name => name.toUpperCase().includes(this.state.search.toUpperCase()))
+            .map(name => (
+              <li key={name}>{name}</li>
+            ))}
+        </ul>
       </div>
     );
   }
--- src/components/CoolSearch.js
+++ src/components/CoolSearch.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './CoolSearch.css'
 
-export default function CoolSearch() {
+export default function CoolSearch(props) {
   return (
     <div className="CoolSearch">
       <div>Search:</div>
       <div>
-        <input type="text" />
+        <input 
+          type="text" 
+          value={props.search} 
+          onChange={e => props.onSearch(e.target.value)}
+        />
       </div>
     </div>
   )
